847c02c046689437cb84ce815dd88a01e73b6acd
coordova <grcordova@gmail.com>
2024-02-03 20:36:30 -0400

    interno.astro - About/Testimonial - scripts

---
+++ src/js/utils.js
@@ -0,0 +1,35 @@
+// mobile menu
+const navTriggerBtn = document.querySelector("#nav-trigger-btn");
+const navMenu = document.querySelector("#nav-menu");
+
+// event listeners
+navTriggerBtn.addEventListener("click", () => {
+	navMenu.classList.toggle("nav-is-open");
+	// console.log("clicked");
+});
+
+// swiper
+const swiper = new Swiper(".swiper", {
+	loop: true,
+	/* autoplay: {
+		delay: 3000,
+	}, */
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	// breakpoints
+	slidesPerView: 3,
+	spaceBetween: 20,
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+		},
+		960: {
+			slidesPerView: 2,
+		},
+		1200: {
+			slidesPerView: 3,
+		},
+	},
+});
