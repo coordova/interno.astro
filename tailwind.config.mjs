b50f579dff92f8a2e714f2541b1b09e21a5800ef
coordova <grcordova@gmail.com>
2024-02-03 20:03:59 -0400

    interno.astro - MainLayout/MainHead Layouts, Header/Footer/Hero/Steps Components & styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		container: {
+			padding: {
+				DEFAULT: "15px",
+			},
+		},
+		screens: {
+			sm: "640px",
+			md: "768px",
+			lg: "960px",
+			xl: "1200px",
+		},
+		fontFamily: {
+			primary: "DM Serif Display",
+			secondary: "Jost",
+		},
+		backgroundImage: {
+			hero: 'url("/assets/hero/bg.jpg")',
+			grid: 'url("/assets/grid.png")',
+		},
+		extend: {
+			colors: {
+				primary: {
+					DEFAULT: "#292f36",
+					hover: "#3434e4a",
+				},
+				secondary: "#4d5053",
+				accent: {
+					DEFAULT: "#cda274",
+					secondary: "#f4f0ec",
+					hover: "#b88c5d",
+				},
+			},
+		},
 	},
 	plugins: [],
-}
+};
